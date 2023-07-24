fba49317a4fc04e9dd900ab2b13a525780c69254
Gideon <gideonkiprono6369@gmail.com>
2023-07-24 14:18:45 +0300

    Added addStudents and addteachers functionality

--- main.js
+++ main.js
@@ -1,14 +1,56 @@
 const express = require("express");
+const mysql = require("mysql");
+var conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "sams",
+});
 
 const app = express();
-//server static ?
+app.use(express.static("public"));
+//Middleware
+app.use(express.urlencoded({ extended: false })); //supply req.body with the form data
+//server static files in express
 const port = process.env.port || 3003;
 app.get("/", (req, res) => {
-  //root route
-  console.log("root route requested!!");
-  res.status(200);
-  res.render("home.ejs");
-  res.send("Home route in the server responding");
+  conn.query("SELECT * FROM  students", (sqlerr1, students) => {
+    if (!sqlerr1) {
+      //continue
+      conn.query("SELECT *FROM teachers", (sqlerr2, teachers) => {
+        if (sqlerr2) {
+          res.send("Database Error Occured");
+        } else {
+          console.log(students);
+          console.log(teachers);
+          res.render("home.ejs", { students: students, teachers: teachers });
+        }
+      });
+    } else {
+      res.send("Database Error Occured");
+    }
+  });
+});
+
+app.get("/anonymous", (req, res) => {
+  conn.query("SELECT COUNT(reg_no)as num FROM students", (sqlerr, dataone) => {
+    if (sqlerr) {
+      console.log(sqlerr);
+    }
+    conn.query(
+      "SELECT COUNT(reg_no)as numm FROM student_attendance WHERE DAY(time_stamp) =11 AND (type ='SpecialIN' OR type='NomalIN')",
+      (errr, dataTwo) => {
+        if (errr) {
+          console.log(errr);
+        }
+        console.log(dataTwo);
+        res.render("home.ejs", {
+          numberOfStudents: dataone[0].num,
+          presentStudents: dataTwo[0].numm,
+        });
+      }
+    );
+  });
 });
 app.get("/news/sports/epl-transfers", (req, res) => {
   //sports route
@@ -25,19 +67,52 @@ app.get("/settings", (req, res) => {
   // res.send("Settings route in the server responding");
 });
 app.get("/newStaff", (req, res) => {
-  res.render("newTeacher.ejs")
+  res.render("newTeacher.ejs");
   console.log("new teacher added");
-})
+});
 app.get("/newStudent", (req, res) => {
-  res.render("newStudent.ejs")
+  res.render("newStudent.ejs");
   console.log("new student added");
-})
-
-
-
-
-
-//app.post("", () => { })
+});
+app.post("/addnewStudent", (req, res) => {
+  //save the data to db
+  console.log(req.body);
+  //redirect user to home/root route
+  conn.query(
+    "INSERT INTO students(reg_no,fullname,class) VALUES(?,?,?)",
+    [req.body.reg_no, req.body.name, Number(req.body.class)],
+    (sqlerr) => {
+      if (sqlerr) {
+        console.log(sqlerr);
+        res.send("A DB error occured while saving new student");
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+});
+app.post("/addStaff", (req, res) => {
+  console.log(req.body);
+  conn.query(
+    "INSERT INTO teachers(tsc_no,full_mame,phone,email,passkey,role) VALUES(?,?,?,?,?,?)",
+    [
+      req.body.tsc,
+      req.body.name,
+      req.body.phone,
+      req.body.email,
+      req.body.pass,
+      req.body.role,
+    ],
+    (sqlErr) => {
+      if (sqlErr) {
+        console.log(sqlErr);
+        res.send("An DB error occured!!");
+      } else {
+        res.redirect("/");
+      }
+    }
+  );
+});
 
 //Assignment--look into environment variables --show how to use .env file in a node project
 app.listen(port, (err) => {
